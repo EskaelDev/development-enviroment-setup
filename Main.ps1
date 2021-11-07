e29d82cdfec820954bc6937ab8727e1d6b81d38f
Eskael <eskaelDev@gmail.com>
2021-11-07 20:52:30 +0100

    Fix Main.ps1

--- Main.ps1
+++ Main.ps1
@@ -5,54 +5,86 @@ function Ask
     )
     do
     {
-        $Answer = Read-Host "Do you want to install/configure $Module? Yes(y) No(n)"
+        $Answer = Read-Host "Do you want to ${Module}? Yes(y) No(n)"
         $Answer = $Answer.ToLower()
         if ($Answer -eq "y")
         {
-            return true
+            return 1
         }
         else
         {
             if ($Answer -eq "n")
             {
-                return false
+                return 0
             }
         }
     } while ($Answer -ne "y" -or $Answer -ne "n")
-    return false
+    return 0
 }
 Clear-Host
-$FiraCode = Ask "Fira Code Font"
-$WindowsTerminal = Ask "Windows Terminal"
-$VSC = Ask "Visual Studio Code"
-$DeskPins = Ask "Desk Pins"
+$FiraCode = Ask "install Fira Code Font"
+$WindowsTerminal = Ask "install and configure Windows Terminal"
+$VSC = Ask "configure Visual Studio Code"
+$DeskPins = Ask "download and install Desk Pins"
 
-if ($FiraCode)
+if ($FiraCode -eq 1)
 {
     # Install Fira Code Nerd Font
-    $PSScriptRoot 
-    $InstallFonts = $PSScriptRoot + "\Font\Install-Fonts.ps1"
-    &$InstallFonts
+    try
+    {
+        $PSScriptRoot 
+        $InstallFonts = $PSScriptRoot + "\Font\Install-Fonts.ps1"
+        &$InstallFonts
+    }
+    catch
+    {
+        Write-Host "Error: " -ForegroundColor Red -NoNewline
+        Write-Host "Fira Code not installed"
+    }
+    
 }
 
-if ($WindowsTerminal)
+if ($WindowsTerminal -eq 1)
 {
     # Setup Windows Terminal
-    $PSScriptRoot 
-    $InstallWt = $PSScriptRoot + "WindowsTerminal\Setup-Windows-Terminal.ps1"
-    &$InstallWt
+    try
+    {
+        $PSScriptRoot 
+        $InstallWt = $PSScriptRoot + "WindowsTerminal\Setup-Windows-Terminal.ps1"
+        &$InstallWt
+    }
+    catch
+    {
+        Write-Host "Error: " -ForegroundColor Red -NoNewline
+        Write-Host "Windows Terminal not configured"
+    }
 }
 
-if ($VSC)
+if ($VSC -eq 1)
 {
     # Install Visual Studio Code
-    $PSScriptRoot 
-    $InstallVSC = $PSScriptRoot + "VisualStudioCode\Setup-Visual-Studio-Code.ps1"
-    &$InstallVSC
+    try
+    {
+        $PSScriptRoot 
+        $InstallVSC = $PSScriptRoot + "VisualStudioCode\Setup-Visual-Studio-Code.ps1"
+        &$InstallVSC
+    }
+    catch
+    {
+        Write-Host "Error: " -ForegroundColor Red -NoNewline
+        Write-Host "Visual Studio Code not configured"
+    }
 }
-
-if ($DeskPins)
+if ($DeskPins -eq 1)
 {
     curl "https://efotinis.neocities.org/downloads/DeskPins-1.32-setup.exe" -o "DeskPins-1.32-setup.exe"
-    Start-Process -FilePath "DeskPins-1.32-setup.exe" -ArgumentList "/S"
+    try
+    {
+        Start-Process -FilePath "DeskPins-1.32-setup.exe" -ArgumentList "/S"
+    }
+    catch
+    {
+        Write-Host "Error: " -ForegroundColor Red -NoNewline
+        Write-Host "Deskpins not installed"
+    }
 }
